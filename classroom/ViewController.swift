06fe76da4de57a33350f9fc9d90a917119a99393
lyubovgolub <lubovlubov.g@gmail.com>
2020-10-08 18:01:02 +0300

    Homework_9

--- classroom/ViewController.swift
+++ classroom/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var myButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
